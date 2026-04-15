d0c43496c4ff3741c3df08b506bd9dee0391378d
adnanshaikh123-7 <skadnan5464@gmail.com>
2026-04-15 13:31:45 +0530

    added requirements and deployment setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$1776ziep4%s1uyyvn%)6!lg811v0rui26^%@+li@9isjc+-^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
